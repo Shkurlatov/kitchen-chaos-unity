8490659749fe4578b0c40727ab25fb0da759187e
Alexander Shkurlatov <shkurlatov80@gmail.com>
2023-05-07 14:00:04 +1200

    Add Plate object

---
+++ Assets/Scripts/PlateCompleteVisual.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateCompleteVisual : MonoBehaviour
+{
+    [Serializable]
+    public struct KitchenObjectSO_GameObject
+    {
+        public KitchenObjectSO KitchenObjectSO;
+        public GameObject GameObject;
+    }
+
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private List<KitchenObjectSO_GameObject> kitchenObjectSOGameObjectList;
+
+    private void Start()
+    {
+        foreach (KitchenObjectSO_GameObject kitchenObjectSOGameObject in kitchenObjectSOGameObjectList)
+        {
+            kitchenObjectSOGameObject.GameObject.SetActive(false);
+        }
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        foreach (KitchenObjectSO_GameObject kitchenObjectSOGameObject in kitchenObjectSOGameObjectList)
+        {
+            if (kitchenObjectSOGameObject.KitchenObjectSO == e.KitchenObjectSO)
+            {
+                kitchenObjectSOGameObject.GameObject.SetActive(true);
+            }
+        }
+    }
+}
