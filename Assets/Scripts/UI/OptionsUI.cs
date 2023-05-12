4fe714d467c3a3489b120783bb62ba30fed04d2d
Alexander Shkurlatov <shkurlatov80@gmail.com>
2023-05-13 06:24:29 +1200

    Add Keys rebinding

--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -11,8 +11,23 @@ public class OptionsUI : MonoBehaviour
     [SerializeField] private Button soundEffectsButton;
     [SerializeField] private Button musicButton;
     [SerializeField] private Button closeButton;
+    [SerializeField] private Button moveUpButton;
+    [SerializeField] private Button moveDownButton;
+    [SerializeField] private Button moveLeftButton;
+    [SerializeField] private Button moveRightButton;
+    [SerializeField] private Button interactButton;
+    [SerializeField] private Button interactAlternateButton;
+    [SerializeField] private Button pauseButton;
     [SerializeField] private TextMeshProUGUI soundEffectsText;
     [SerializeField] private TextMeshProUGUI musicText;
+    [SerializeField] private TextMeshProUGUI moveUpText;
+    [SerializeField] private TextMeshProUGUI moveDownText;
+    [SerializeField] private TextMeshProUGUI moveLeftText;
+    [SerializeField] private TextMeshProUGUI moveRightText;
+    [SerializeField] private TextMeshProUGUI interactText;
+    [SerializeField] private TextMeshProUGUI interactAlternateText;
+    [SerializeField] private TextMeshProUGUI pauseText;
+    [SerializeField] private Transform pressToRebindKeyTransform;
 
     private void Awake()
     {
@@ -21,6 +36,14 @@ public class OptionsUI : MonoBehaviour
         soundEffectsButton.onClick.AddListener(SoundEffectsClick);
         musicButton.onClick.AddListener(MusicClick);
         closeButton.onClick.AddListener(CloseClick);
+
+        moveUpButton.onClick.AddListener(MoveUpClick);
+        moveDownButton.onClick.AddListener(MoveDownClick);
+        moveLeftButton.onClick.AddListener(MoveLeftClick);
+        moveRightButton.onClick.AddListener(MoveRightClick);
+        interactButton.onClick.AddListener(InteractClick);
+        interactAlternateButton.onClick.AddListener(InteractAlternateClick);
+        pauseButton.onClick.AddListener(PauseClick);
     }
 
     private void Start()
@@ -29,6 +52,7 @@ public class OptionsUI : MonoBehaviour
 
         UpdateVisual();
 
+        HidePressToRebindKey();
         Hide();
     }
 
@@ -48,16 +72,59 @@ public class OptionsUI : MonoBehaviour
         MusicManager.Instance.ChangeVolume();
         UpdateVisual();
     }
-    
+
     private void CloseClick()
     {
         Hide();
+    }
+
+    private void MoveUpClick()
+    {
+        RebindBinding(GameInput.Binding.Move_Up);
+    }
+
+    private void MoveDownClick()
+    {
+        RebindBinding(GameInput.Binding.Move_Down);
+    }
+
+    private void MoveLeftClick()
+    {
+        RebindBinding(GameInput.Binding.Move_Left);
+    }
+
+    private void MoveRightClick()
+    {
+        RebindBinding(GameInput.Binding.Move_Right);
+    }
+
+    private void InteractClick()
+    {
+        RebindBinding(GameInput.Binding.Interact);
+    }
+
+    private void InteractAlternateClick()
+    {
+        RebindBinding(GameInput.Binding.InteractAlternate);
+    }
+
+    private void PauseClick()
+    {
+        RebindBinding(GameInput.Binding.Pause);
     }
 
     private void UpdateVisual()
     {
         soundEffectsText.text = "Sound Effect: " + Mathf.Round(SoundManager.Instance.GetVolume() * 10f);
         musicText.text = "Music: " + Mathf.Round(MusicManager.Instance.GetVolume() * 10f);
+
+        moveUpText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Up);
+        moveDownText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Down);
+        moveLeftText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Left);
+        moveRightText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Right);
+        interactText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Interact);
+        interactAlternateText.text = GameInput.Instance.GetBindingText(GameInput.Binding.InteractAlternate);
+        pauseText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Pause);
     }
 
     public void Show()
@@ -68,5 +135,25 @@ public class OptionsUI : MonoBehaviour
     private void Hide()
     {
         gameObject.SetActive(false);
+    }
+
+    private void ShowPressToRebindKey()
+    {
+        pressToRebindKeyTransform.gameObject.SetActive(true);
+    }
+
+    private void HidePressToRebindKey()
+    {
+        pressToRebindKeyTransform.gameObject.SetActive(false);
+    }
+
+    private void RebindBinding(GameInput.Binding binding)
+    {
+        ShowPressToRebindKey();
+        GameInput.Instance.RebindBinding(binding, () =>
+        {
+            HidePressToRebindKey();
+            UpdateVisual();
+        });
     }
 }
